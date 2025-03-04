612b17a1b6c8e3c7058db0668ac9caaeb802181e
trinhee <ethan.trinh4@gmail.com>
2025-03-04 02:47:26 -0500

    changed to relative path

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from '@/components/Navbar'
-import Hero from '@/components/Hero'
-import About from '@/components/About'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import About from '../components/About'
 
 const page = () => {
   return (
