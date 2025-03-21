7625b185a93baa3c876b388d96ec675849b7a072
trinhee <ethan.trinh4@gmail.com>
2025-03-20 21:07:08 -0400

    env

--- backend/api/index.js
+++ backend/api/index.js
@@ -9,8 +9,11 @@ app.use(express.json());
 
 // Instantiate the Postgres pool
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
 
 // Redirect `/` to `/api`
